d8bbc02f375e9f9a2d78fff89181bda8838a4ee0
Stavros Tsirlidis <stavros.tsirlidis@student.kyh.se>
2019-07-25 17:58:33 +0200

    small changes made

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -7,6 +7,20 @@ return(
 <styled.container >
   <styled.heroBox>
 
+    <styled.heroBoxTitle>
+        <styled.heroBoxTitleTransparansBox>
+            <styled.logoLine>
+                
+            </styled.logoLine>
+            <h1>SIRLIDI</h1>
+            <styled.logo>
+                @@
+            </styled.logo>
+        </styled.heroBoxTitleTransparansBox>
+    </styled.heroBoxTitle>
+
+    Created to Create
+
   </styled.heroBox>
 </styled.container>
 
--- src/components/hero/heroStyle.js
+++ src/components/hero/heroStyle.js
@@ -9,25 +9,50 @@ export const container = styled.div`
  justify-content:center;
  align-items:center;
 
- 
-
- h1{
-     color:blue;
-   };
 
 `
 
 export const heroBox = styled.div`
  width:50%;
  height:65vh;
- color:blueviolet;
- border: 25px solid white;
- 
-
- h1{
-     color:blue;
-   };
+ border: 1px solid white;
+ display:flex;
+ align-items:center;
+ flex-direction:column;
+`
+export const heroBoxTitle = styled.div`
+ width:100%;
+ height:50%;
+ border: 1rem solid white;
+ display:flex;
+ justify-content:center;
+ align-content:center;
+ padding:15px;
 
 `
+export const heroBoxTitleTransparansBox = styled.div`
+ width:100%;
+ height:100%;
+ background-color:#3f3f3f3b;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+ color:white;
 
-    
+ h1{
+  font-size:8em;
+ }
+ 
+`
+
+export const logo = styled.div`
+ width:100%;
+ height:100%;
+ border: 1px solid green;
+`
+export const logoLine = styled.div`
+ width:100%;
+ height:100%;
+ border: 5px solid green;
+ text-align:right;
+` 
